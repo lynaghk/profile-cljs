db34f8e7a66a636bdc0d97a15fc033838222a24d
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-05 20:27:29 -0800

    WIP: Trying to import JavaScript-in-a-JAR generated by cassowary-coffee.

--- make.sh
+++ make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 CLJSC_CP=''
-for next in src/clj: lib/*: vendor/clojurescript/lib/*: vendor/clojurescript/src/clj: vendor/clojurescript/src/cljs; do
+for next in src/clj: ../cassowary-coffee/cassowary-0.0.1-SNAPSHOT.jar: lib/*: vendor/clojurescript/lib/*: vendor/clojurescript/src/clj: vendor/clojurescript/src/cljs; do
   CLJSC_CP=$CLJSC_CP$next
 done
 
 rm -rf out
 mkdir -p out
-java -server -cp $CLJSC_CP clojure.main vendor/clojurescript/bin/cljsc.clj src/cljs {:optimizations :advanced} > out/compiled.js
+java -server -cp $CLJSC_CP clojure.main compile.clj
 
 phantomjs src/run.js
