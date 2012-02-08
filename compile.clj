223798bc9ee5bd30acaf0bf225861a37df89551e
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-07 20:26:24 -0800

    Writeup some notes in the compile file, catch exceptions in cljs test file. Results from running:
    ClojureScript apparently only picks up dependencies from naked JavaScript---i.e., not wrapped in
    
        (function(){ ... }).call()

--- compile.clj
+++ compile.clj
@@ -1,6 +1,8 @@
 (require '[cljs.closure :as closure])
 
-(closure/build "src/cljs/test-jar.cljs" {:optimizations :simple
+;;This doesn't work, need to use :libs
+;;The reason Pinot got away with using goog.jar is because goog/deps.js is a hardcoded lib in the ClojureScript compiler.
+#_(closure/build "src/cljs/test-jar.cljs" {:optimizations :simple
                                          :output-dir "no-libs/"
                                          :output-to "no-libs/compiled.js"})
 
@@ -8,6 +10,11 @@
 (closure/build "src/cljs/test-jar.cljs" {:optimizations :simple
                                          :output-dir "with-libs/"
                                          :output-to "with-libs/compiled.js"
-                                         :libs ["Naked.js"
-                                                "WithScope.js"
-                                                "WithoutScope.js"]})
+                                         ;;Empty string works just as well as list of JS files
+                                         ;;Comment on clojurescript/src/clj/cljs/closure.clj#js-dependencies says that :libs works with path to directories, so "" must be resolving to the JAR root.
+                                         :libs [""] 
+                                         ;; :libs ["Naked.js"
+                                         ;;        "NakedScoped.js"
+                                         ;;        "WithScope.js"
+                                         ;;        "WithoutScope.js"]
+                                         })
--- src/cljs/test-jar.cljs
+++ src/cljs/test-jar.cljs
@@ -1,10 +1,26 @@
 (ns test-jar
   (:require [Naked :as naked]
+            [NakedScoped :as naked-scoped]
             [WithScope :as with-scope]
             [WithoutScope :as without-scope]))
 
 (defn p [x] (.log js/console x))
+(try
+  (p (naked/echo "grr"))
+  (catch js/Error e
+    (p "Naked without scope: NOPE")))
 
-(p (naked/echo "grr"))
-(p (with-scope/echo "grr"))
-(p (without-scope/echo "grr"))
+(try
+  (p (naked-scoped/echo "grr"))
+  (catch js/Error e
+    (p "NakedScoped without scope: NOPE")))
+
+(try
+  (p (with-scope/echo "grr"))
+  (catch js/Error e
+    (p "wrapped-with-scope: NOPE")))
+
+(try
+  (p (without-scope/echo "grr"))
+  (catch js/Error e
+    (p "wrapped-without-scope: NOPE")))
