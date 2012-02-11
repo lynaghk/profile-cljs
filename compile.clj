d5143f71cfe2a6d53c86bb41c98737223e54a801
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-10 19:17:27 -0800

    Compile one file at a time...

--- compile.clj
+++ compile.clj
@@ -1,5 +1,5 @@
 (require '[cljs.closure :as closure])
 
-(closure/build "src/cljs" {:optimizations :simple
-                           :output-dir "out/"
-                           :output-to "out/compiled.js"})
+(closure/build "src/cljs/test1.cljs" {:optimizations :simple
+                                      :output-dir "out/"
+                                      :output-to "out/compiled.js"})
