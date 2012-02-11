bb6016799d2c3516c012b0163500454c5c653474
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-11 12:57:26 -0800

    Cassowary JS & CLJS wrapper working from JAR in simple optimizations.
    Need clean up Cassowary vendor JS to make them more Closure-compatible.

--- compile.clj
+++ compile.clj
@@ -1,13 +1,14 @@
 (require '[cljs.closure :as closure])
 
-(closure/build "src/cljs/cassowary.cljs" {:optimizations :advanced
-                                          :output-dir "out/advanced/"
-                                          :output-to "out/advanced/compiled.js"
-                                          :libs [""]})
+#_(closure/build "src/cljs/cassowary.cljs" {:optimizations :advanced
+                                            :output-dir "out/advanced/"
+                                            :output-to "out/advanced/compiled.js"
+                                            :libs [""]})
 
 (closure/build "src/cljs/cassowary.cljs" {:optimizations :simple
-                                          :output-dir "out/simple/"
-                                          :output-to "out/simple/compiled.js"
+                                          :pretty-print true
+                                          :output-dir "out/"
+                                          :output-to "out/compiled.js"
                                           :libs [""]})
 
 
--- src/cljs/cassowary.cljs
+++ src/cljs/cassowary.cljs
@@ -1,23 +1,42 @@
 (ns cassowary
+  (:refer-clojure :exclude [+ - =])
   (:use-macros [util :only [profile p]])
-  (:require [Cl :as Cl]
-            [Cl.CL :as CL])
-  
-  #_(:use [cassowary.core :only [+ - = cvar constrain! stay! simplex-solver]]))
+  (:use [cassowary.core :only [+ - = cvar constrain! stay! simplex-solver]]))
 
 
 (defn *print-fn* [x]
   (.log js/console x))
 
-(p Cl/version)
-(p CL/GEQ)
-(p (CL/approx 1 1))
 
-#_(let [solver (simplex-solver)
-      x  (cvar 0)
-      two-x  (cvar 0)]
+(let [height 200, width 800, max-radius 40
+      solver  (simplex-solver)
+      spacing (cvar 0) ;;The spacing between circles (to be solved for)
+      circles (repeatedly 10 #(hash-map :r  (cvar (* max-radius (rand)))
+                                        :cx (cvar 0)
+                                        :cy (cvar (/ height 2))))]
 
-  (profile "solve simple constraint"
-           (constrain! solver (= two-x (* 2 x)))
-           (stay! solver x 10)
-           (p (.value x))))
+  ;;The circle radii and vertical positions are constants
+  (doseq [c circles]
+    (stay! solver (:r c))
+    (stay! solver (:cy c)))
+
+  ;;Spacing between first circle and the wall
+  (constrain! solver (= 0 (- (:cx (first circles))
+                             (:r (first circles))
+                             spacing)))
+
+  ;;Spacing between each pair of neighboring circles
+  (doseq [[left right] (partition 2 1 circles)]
+    (constrain! solver (= spacing (- (:cx right)
+                                     (:r right)
+                                     (+ (:cx left) (:r left))))))
+
+  ;;Spacing between last circle and the wall
+  (constrain! solver (= spacing (- width
+                                   (:cx (last circles))
+                                   (:r (last circles)))))
+
+  (p (.value (:cx (first circles))))
+
+  )
+
