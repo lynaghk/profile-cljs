bbea86ffca5872bec4c1ddd8e075ca95e19e7419
Kevin J. Lynagh <kevin@keminglabs.com>
2012-04-30 08:37:34 -0700

    Compare clojure.string#join with js array join. The latter is about 3 times faster.

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject profile-cljs "0.0.1-SNAPSHOT"
   :description "Look at ClojureScript speed with respect to DOM manipulation."
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.match "0.2.0-alpha9"]
                  
                  [com.keminglabs/c2 "0.1.0-beta2-SNAPSHOT"]
                  [com.keminglabs/vomnibus "0.3.0"]]
   
   :plugins [[lein-cljsbuild "0.1.8"]]
-  :cljsbuild {:builds {:test {:source-path "src/cljs/seq_test"
+  :cljsbuild {:builds {:test {:source-path "src/cljs/join_test"
                               :compiler {:output-to "public/main.js"
                                          :optimizations :simple
                                          :pretty-print true}}}}
