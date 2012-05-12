6e06c801a730bbce82d7950b22c0e6bce68794f9
Kevin J. Lynagh <kevin@keminglabs.com>
2012-05-12 15:18:20 -0700

    Add list and vector tests from clojurescript/benchmark/cljs/benchmark_runner.cljs.

--- src/cljs/microbenchmarks/seqs.cljs
+++ src/cljs/microbenchmarks/seqs.cljs
@@ -6,3 +6,18 @@
 
 (profile "map inc"
          (doall (map inc (range 100000))))
+
+(let [l (list 1 2 3)]
+  (profile {:group "lists" :n 100000} (satisfies? ISeq l))
+  (profile {:group "lists" :n 100000} (first l))
+  (profile {:group "lists" :n 100000} (-first l))
+  (profile {:group "lists" :n 100000} (rest l))
+  (profile {:group "lists" :n 100000} (next l)))
+
+(let [v [1 2 3]]
+  (profile {:group "vectors" :n 100000} (satisfies? ISeq v))
+  (profile {:group "vectors" :n 100000} (first v))
+  (profile {:group "vectors" :n 100000} (rest v))
+  (profile {:group "vectors" :n 100000} (next v)))
+
+
