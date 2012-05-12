9e7d0ab6e6f5b93d4df6bba5ac4e068121989ff1
Kevin J. Lynagh <kevin@keminglabs.com>
2012-05-12 14:02:29 -0700

    Update project.clj deps.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject profile-cljs "0.0.1-SNAPSHOT"
   :description "Compare ClojureScript & JS performance"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.match "0.2.0-alpha9"]
 
-                 [com.keminglabs/c2 "0.1.0-beta2-SNAPSHOT"]
+                 [com.keminglabs/c2 "0.1.1-SNAPSHOT"]
                  [com.keminglabs/vomnibus "0.3.0"]]
 
-  :plugins [[lein-cljsbuild "0.1.8"]]
+  :plugins [[lein-cljsbuild "0.1.9"]]
   
   :cljsbuild {:builds {:simple {:source-path "src/cljs/microbenchmarks"
                                 :compiler {:output-to "public/simple.js"
@@ -19,6 +19,7 @@
                                              :optimizations :simple}}}}
   :source-paths ["src/clj" "src/cljs"
                  ;;If you want to reference local ClojureScript checkout instead of whatever lein cljsbuild is using
-                 "vendor/clojurescript/src/clj" "vendor/clojurescript/src/cljs"]
+                 ;;"vendor/clojurescript/src/clj" "vendor/clojurescript/src/cljs"
+                 ]
 
   :min-lein-version "2.0.0")
