9ed89b331cffcdbe6b81272272a50df6c69cabc1
Kevin J. Lynagh <kevin@keminglabs.com>
2012-05-12 15:17:51 -0700

    Profile macro supports optional :n opts key for number of repetitions of body.

--- src/clj/util/macros.clj
+++ src/clj/util/macros.clj
@@ -14,12 +14,13 @@
 
 (defmacro profile [opts & body]
   (let [timing (merge (if (string? opts) {:group opts} opts)
-                    {:lang "cljs"
-                     :src (join "\n" (map pr-str body))})]
+                      {:lang "cljs"
+                       :src (join "\n" (map pr-str body))})
+        n (get timing :n 1)]
     `(let [start# (.getTime (js/Date.))
-           ret# (do ~@body)
+           ret# (dotimes [_# ~n] ~@body)
            dt# (- (.getTime (js/Date.)) start#)]
-       
+
        (js/add_timing (apply ~'js-obj
                              (flatten (map (fn [[k# v#]] [(name k#) v#])
                                            (assoc ~timing :dt dt#)))))
