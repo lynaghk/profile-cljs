878e9dbb6840ee32c38f659bbe5fe2a7ec0621a3
Kevin J. Lynagh <kevin@keminglabs.com>
2012-05-23 16:59:34 -0700

    Look at sets.

--- src/cljs/microbenchmarks/maps.cljs
+++ src/cljs/microbenchmarks/maps.cljs
@@ -4,11 +4,21 @@
 (def a-map {:a 1 :b 2 :c 3})
 (defrecord TestRecord [a b c])
 (def a-record (TestRecord. 1 2 3))
+(def a-set #{:a :b :c :d :e})
 
 (profile {:group "field access" :n 10000} (:a a-map))
 (profile {:group "field access" :n 10000} (a-map :a))
 (profile {:group "field access" :n 10000} (get a-map :a))
+(profile {:group "field access" :n 10000} (contains? a-map :a))
+
 (profile {:group "field access" :n 10000} (:a a-record))
+(profile {:group "field access" :n 10000} (a-record :a))
+(profile {:group "field access" :n 10000} (get a-record :a))
+(profile {:group "field access" :n 10000} (contains? a-record :a))
+
+(profile {:group "field access" :n 10000} (:a a-set))
+(profile {:group "field access" :n 10000} (a-set :a))
+(profile {:group "field access" :n 10000} (contains? a-set :a))
 
 (profile {:group "destructuring" :n 10000}
          (let [{:keys [a b c]} a-map]))
