d4f3a1d08f926e588761c31a55ff2c04c8f94ea1
Kevin J. Lynagh <kevin@keminglabs.com>
2012-05-23 21:16:42 -0700

    WIP: cljs->js conversion.

--- src/cljs/c2_benchmarks/hiccup.cljs
+++ src/cljs/c2_benchmarks/hiccup.cljs
@@ -46,10 +46,10 @@
   "Determines namespace URI from tag string, defaulting to xhtml. Returns [nsp tag]."
   [tag-str]
   (let [tag-str "div"]
-           (if (.match tag-str #":")
-             (let [[nsp tag] [(string/split tag-str #":")]]
-               [(get xmlns (keyword nsp) nsp) (keyword tag)])
-             [(if (.match tag-str re-svg-tags) (:svg xmlns) (:xhtml xmlns)) tag-str])))
+    (if (.match tag-str #":")
+      (let [[nsp tag] [(string/split tag-str #":")]]
+        [(get xmlns (keyword nsp) nsp) (keyword tag)])
+      [(if (.match tag-str re-svg-tags) (:svg xmlns) (:xhtml xmlns)) tag-str])))
 
 (defn unify? [m]
   (= c2-benchmarks.hiccup.Unify (type m)))
@@ -73,7 +73,7 @@
                             (map canonicalize %)
                             [(canonicalize %)])
                          children)]
-    
+
     (Hiccup. nsp tag attr children)))
 
 (defn canonicalize-hiccup-without-attr [v]
@@ -93,20 +93,62 @@
    (if (map? (second? x))
      (canonicalize-hiccup-with-attr x)
      (canonicalize-hiccup-without-attr x))
-   
+
    (hiccup? x) x
    (unify? x)  x))
 
-(profile {:group "regex" :n 1000} (re-matches re-tag "div#with-id.and-class"))
-(profile {:group "regex" :n 1000} (.match "div#with-id.and-class" re-tag))
-(profile {:group "regex" :n 1000} (re-find #":" "abc:def"))
-(profile {:group "regex" :n 1000} (string/split "abc:def" #":"))
+;; (profile {:group "regex" :n 1000} (re-matches re-tag "div#with-id.and-class"))
+;; (profile {:group "regex" :n 1000} (.match "div#with-id.and-class" re-tag))
+;; (profile {:group "regex" :n 1000} (re-find #":" "abc:def"))
+;; (profile {:group "regex" :n 1000} (string/split "abc:def" #":"))
 
 (profile {:group "hiccup" :n 1000}
          (canonicalize [:div [:span] [:span]]))
+(profile {:group "hiccup" :n 1000}
+         [:div [:span] [:span]])
 
-;; (profile {:group "hiccup" :n 1000}
-;;          (canonicalize [:div {:a 1}]))
 
-;; (profile {:group "hiccup" :n 1000}
-;;          (canonicalize [:div {:a 1} [:span "grr"]]))
+
+(defn clj->js
+  "Recursively transforms ClojureScript maps into Javascript objects,
+   other ClojureScript colls into JavaScript arrays, and ClojureScript
+   keywords into JavaScript strings."
+  [x]
+  (cond
+   (string? x)  x
+   (keyword? x) (name x)
+   (map? x)     (reduce (fn [o [k v]]
+                          (let [key (clj->js k)]
+                            (when-not (string? key)
+                              (throw "Cannot convert; JavaScript map keys must be strings"))
+                            (aset o key (clj->js v))
+                            o))
+                        (js-obj) x)
+   (coll? x)    (let [a (array)]
+                  (doseq [item x]
+                    (.push a (clj->js v))))
+   :else x))
+
+(profile {:group "hiccup" :n 1000}
+         (clj->js [:div [:span] [:span]]))
+
+(profile {:group "hiccup" :n 1000}
+         (clj->js [:div [:span] [:span]]))
+
+(let [x {:a 1 :b 2}]
+  (profile {:group "conversion" :n 1000}
+           (reduce (fn [o [k v]]
+                     (let [key (clj->js k)]
+                       (when-not (string? key)
+                         (throw "Cannot convert; JavaScript map keys must be strings"))
+                       (aset o key (clj->js v))
+                       o))
+                   (js-obj) x))
+
+  (profile {:group "conversion" :n 1000}
+           (let [o (js-obj)]
+             (doseq [[k v] x]
+               (let [key (clj->js k)]
+                 (when-not (string? key)
+                   (throw "Cannot convert; JavaScript map keys must be strings"))
+                 (aset o key (clj->js v)))))))
