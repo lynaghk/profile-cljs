def5f6692da179d18a476bb221baf020c042741c
Kevin J. Lynagh <kevin@keminglabs.com>
2012-05-23 17:43:40 -0700

    Nicer JS profile scaffolding.

--- src/js/microbenchmarks/field_access.js
+++ src/js/microbenchmarks/field_access.js
@@ -1,13 +1,9 @@
 var a_map = {a: 1};
 
-profile("field access", function(){
-  for (var i = 0; i <= 10000; i++){
+profile({n:10000, group:"field access"}, function(){
     a_map["a"];
-  }
 });
 
-profile("field access", function(){
-  for (var i = 0; i <= 10000; i++){
+profile({n:10000, group:"field access"}, function(){
     a_map.a;
-  }
 });
